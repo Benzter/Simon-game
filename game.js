8cfce0b8568886678f6c622ac2ffd6414dfdeea8
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2022-12-24 21:06:48 +0530

    added javascript using jqeury to manipulate the game

---
+++ game.js
@@ -0,0 +1,80 @@
+var buttonColors = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).keydown(function(){
+   if(!started){
+    $("h1").text("Level "+level);
+    nextSequence();
+    started = true;
+   }
+    
+})
+
+
+function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("h1").text("Level "+level);
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChoosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChoosenColor);
+
+    $("#"+randomChoosenColor).fadeOut(100).fadeIn(100);
+
+    playSound(randomChoosenColor);
+}
+
+$(".btn").click(function(){
+    
+    var userChoosenColor = $(this).attr("id");
+    userClickedPattern.push(userChoosenColor);
+    
+    playSound(userChoosenColor);
+    animatePress(userChoosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("success");
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+    }
+    
+    }
+    else{
+        console.log("wrong")
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+        $("body").removeClass("game-over");
+        },200);
+
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
